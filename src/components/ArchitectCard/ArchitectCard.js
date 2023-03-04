5d723d4478fd95435e7f04656942cd9e0af5b265
jaison080 <jaisondennis080@gmail.com>
2023-03-04 22:13:58 +0530

    Landing Page COmplete

--- src/components/ArchitectCard/ArchitectCard.js
+++ src/components/ArchitectCard/ArchitectCard.js
@@ -3,7 +3,7 @@ import styles from "./ArchitectCard.module.css";
 
 function ArchitectCard() {
   return (
-    <div className={styles.architectcard}>
+    <div className={styles.architectcard} data-aos="fade-up">
       <div className={styles.img}>
         <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
       </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
